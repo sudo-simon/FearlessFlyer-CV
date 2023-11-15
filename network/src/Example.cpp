f945edc2bc4fe799fcbb3ab2bdb6b9d69ef1664e
sudo-simon <simone.3556@gmail.com>
2023-11-15 18:11:22 +0100

    develop branch up-to-date

---
+++ network/src/Example.cpp
@@ -0,0 +1,48 @@
+#include "NetConf.hpp"
+
+int VideoStream(std::string rtmpAddress){
+
+    cv::VideoCapture cap(0); 
+    if (!cap.isOpened()) { 
+        return -1;
+    }
+
+    while (true) {
+        cv::Mat frame;
+        cap >> frame; 
+        if (frame.empty())
+            break; 
+
+        cv::imshow("RTMP", frame);
+
+        if (cv::waitKey(30) >= 0){
+            break;
+        }
+    }
+    cap.release();
+    return 0;
+}
+
+int main(){
+
+    // Installa nginx
+
+    std::string ip;
+    std::cout << "Network IP address: ";
+    std::cin >> ip; 
+
+    NetConf network(ip);
+    std::cout << network.RTMPconfig();
+    network.ServerStart();
+    network.BindRtmpLink();
+    std::cout << "RTMP address: " << network.GetRtmpLink()<< std::endl;
+
+    // rtmp://192.168.1.123:1935/live/test
+    int res = VideoStream(network.GetRtmpLink());
+    if(res == -1){
+        std::cerr << "VideoStream() ERROR." << std::endl;
+    }
+
+    network.ServerStop();
+}
+
