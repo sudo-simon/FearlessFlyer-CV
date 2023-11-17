9ab512d3adfc2854fe2b279edbbdfb447397d867
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2023-11-17 17:42:12 +0100

    Dear ImGui (glfw+opengl3)
    To do:
    - import glfw, opengl3 libs
    - create a CMakeLists
    - Study the example

---
+++ src/imguiExample.cpp
@@ -0,0 +1,11 @@
+#include "imgui/imgui.h"
+#include "imgui/glfw_impl/imgui_impl_glfw.h"
+#include "imgui/opengl3_impl/imgui_impl_opengl3.h"
+//import glfw
+//import gl3 (glut or glew)
+
+int main(){
+
+    //Anlayze the examples
+
+}
