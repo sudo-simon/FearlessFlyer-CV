6e24c8afeb6fb415f24e4957c70370daf521bdde
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2023-11-18 02:21:00 +0100

    fix directories part 2

---
+++ modules/network/install/nginxSetup.sh
@@ -0,0 +1,5 @@
+echo "Installation and setup nginx server :)"
+sudo apt update
+sudo add-apt-repository universe
+sudo apt install libnginx-mod-rtmp ffmpeg
+echo "Done :)"
---
+++ src/modules/network/src/Example.cpp
@@ -0,0 +1,50 @@
+#include "NetConf.hpp"
+
+int VideoStream(std::string rtmpAddress){
+
+    cv::VideoCapture cap(rtmpAddress); 
+    if (!cap.isOpened()) { 
+        return -1;
+    }
+
+    while (true) {
+        cv::Mat frame;
+        cap >> frame; 
+        if (frame.empty())
+            break; 
+
+        cv::imshow("RTMP", frame);
+
+        if (cv::waitKey(30) >= 0){
+            break;
+        }
+    }
+    cap.release();
+    return 0;
+}
+
+int main(){
+
+    // Installa nginx
+
+    NetConf network;
+    network.BindIp();
+    network.SearchBindPort();
+    network.RTMPconfig();
+    network.ServerStart();
+    network.BindRtmpLink("test");
+    std::cout << "RTMP address: " << network.GetExternalRtmpLink()<< std::endl;
+
+
+    std::cout << "Press <ENTER> to start capturing." << std::endl;
+    std::cin.ignore();
+    std::cout << "Capturing..." << std::endl;
+
+    int res = VideoStream(network.GetInternalRtmpLink());
+    if(res == -1){
+        std::cerr << "VideoStream() ERROR." << std::endl;
+    }
+
+    network.ServerStop();
+}
+
